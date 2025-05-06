a28295667f10b9724df5ebce3f2dbb682c5c5be2
mohammed elshafey <mohammeddddd069@gmail.com>
2025-05-06 17:57:05 +0300

    initial

--- design0/src/test.vhd
+++ design0/src/test.vhd
@@ -3,20 +3,14 @@ use ieee.std_logic_1164.all;
 
 
 entity test is
-	--generic
 	
-	--port 
-	port(
-	  x:in std_logic :='0';
-	  y:out std_ulogic :='0'
-	);
 	
 end entity;
 
 architecture sim of test is
---declaration of architecture
+
 begin
-	--concurent statement
+	
 	
 
 end architecture;	
